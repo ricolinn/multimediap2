9952c431bd2b3f74c5dd7adc69dfc335cd39dec0
Ricardo Carmona <ricky99013@gmail.com>
2020-12-13 03:32:40 +0100

    feat: added Movie class

---
+++ src/web_scraping/Movie.java
@@ -0,0 +1,109 @@
+package web_scraping;
+
+public class Movie {
+
+	String id, link, title, genre, poster, director, summary, keywords, cast;
+	float score;
+	int year;
+	
+	public Movie(String id, String link, String title, String genre, String poster, float score, int year) {
+		super();
+		this.id = id;
+		this.link = link;
+		this.title = title;
+		this.genre = genre;
+		this.poster = poster;
+		this.score = score;
+		this.year = year;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getLink() {
+		return link;
+	}
+
+	public void setLink(String link) {
+		this.link = link;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	public String getPoster() {
+		return poster;
+	}
+
+	public void setPoster(String poster) {
+		this.poster = poster;
+	}
+
+	public float getScore() {
+		return score;
+	}
+
+	public void setScore(float score) {
+		this.score = score;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public String getDirector() {
+		return director;
+	}
+
+	public void setDirector(String director) {
+		this.director = director;
+	}
+
+	public String getSummary() {
+		return summary;
+	}
+
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+
+	public String getKeywords() {
+		return keywords;
+	}
+
+	public void setKeywords(String keywords) {
+		this.keywords = keywords;
+	}
+
+	public String getCast() {
+		return cast;
+	}
+
+	public void setCast(String cast) {
+		this.cast = cast;
+	}
+	
+	
+}
